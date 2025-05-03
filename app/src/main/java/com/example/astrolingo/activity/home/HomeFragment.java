ae433e1ac5ebbc2f83dc0bb022436f30fb4e6989
VStorm <tranvubao2004@gmail.com>
2025-05-04 02:17:04 +0700

    edit layout

--- app/src/main/java/com/example/astrolingo/activity/home/HomeFragment.java
+++ app/src/main/java/com/example/astrolingo/activity/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.astrolingo.activity.home;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.astrolingo.R;
 
 
 public class HomeFragment extends Fragment {
 
+    TextView leaderboard_seeMore;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // init leaderboard
+        leaderboard_seeMore = view.findViewById(R.id.leaderboard_see_more);
+        leaderboard_seeMore.setPaintFlags(leaderboard_seeMore.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        return view;
     }
 }
