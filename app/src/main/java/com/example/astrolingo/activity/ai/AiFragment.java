878644e1f9527dbac01cab5aaa47a281b8eee4c4
VStorm <tranvubao2004@gmail.com>
2025-05-29 16:55:51 +0700

    final commit

--- app/src/main/java/com/example/astrolingo/activity/ai/AiFragment.java
+++ app/src/main/java/com/example/astrolingo/activity/ai/AiFragment.java
@@ -1,22 +1,31 @@
 package com.example.astrolingo.activity.ai;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.astrolingo.R;
 import com.example.astrolingo.apdapter.ai.messageChatboxAdapter;
+import com.example.astrolingo.api.ChatBoxApi;
 import com.example.astrolingo.domain.ai.messageChatbox;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -62,6 +71,7 @@ public class AiFragment extends Fragment {
     private ImageView sentMessage;
     private messageChatboxAdapter messageAdapter;
     private ArrayList<messageChatbox> list_message = new ArrayList<>();
+    private ProgressBar process_bar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -79,6 +89,9 @@ public class AiFragment extends Fragment {
         lv_chatbox = view.findViewById(R.id.lv_chatbox);
         editText = view.findViewById(R.id.editText);
         sentMessage = view.findViewById(R.id.sentMessage);
+        process_bar = view.findViewById(R.id.process_bar);
+
+        process_bar.setVisibility(View.GONE);
 
         // init listview
         initValueListView();
@@ -96,10 +109,50 @@ public class AiFragment extends Fragment {
                 messageAdapter.notifyDataSetChanged();
             }
 
+            process_bar.setVisibility(View.VISIBLE);
+            editText.setEnabled(false);
+
+            chatAI(mess, getContext());
+
             editText.setText("");
         });
 
         return view;
+    }
+
+    private void chatAI(String mess, Context context) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("query", mess);
+
+        ChatBoxApi.chat(
+                params,
+                context,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Xử lý khi thành công
+                        Log.d("API_SUCCESS", response.toString());
+
+                        String aiChat_text = response.optString("response");
+
+                        messageChatbox aiChat = new messageChatbox(aiChat_text, false);
+                        list_message.add(aiChat);
+                        messageAdapter.notifyDataSetChanged();
+
+                        process_bar.setVisibility(View.GONE);
+                        editText.setEnabled(true);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Xử lý khi có lỗi
+                        //Log.e("API_ERROR", error.toString());
+
+                        process_bar.setVisibility(View.GONE);
+                        editText.setEnabled(true);
+                    }
+                });
     }
 
     private void initValueListView() {
